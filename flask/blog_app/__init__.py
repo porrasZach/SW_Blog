12a1216d87099b161e95ce9b769087c18ee03d93
backfromsolaris <porraszach@gmail.com>
2021-08-30 22:43:54 -0500

    react & flask are talking via /time test route

--- flask/blog_app/__init__.py
+++ flask/blog_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from .authentication.routes import auth
 from .site.routes import site
+import time
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ app.config.from_object(Config)
 
 app.register_blueprint(site)
 app.register_blueprint(auth)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.ctime()}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() =>{
+    fetch('/time').then(res => res.json()).then(data =>{
+      setCurrentTime(data.time);
+    })
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
