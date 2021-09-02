6dceb86805c323bba5b4102ec9cca1cf8ca085c8
backfromsolaris <porraszach@gmail.com>
2021-09-01 23:59:37 -0500

    added /api/books practice call

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import logo from './Assets/Images/star_wars_logo.png'
-import { useState } from 'react';
+// import logo from './Assets/Images/star_wars_logo.png'
+// import { useState } from 'react';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+// function App() {
+//   const [currentTime, setCurrentTime] = useState(0);
 
+//   useEffect(() =>{
+//     fetch('/time').then(res => res.json()).then(data =>{
+//       setCurrentTime(data.time);
+//     })
+//   }, []);
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Welcome to the front-end of this project!
+//         </p>
+//         <p>The current time is {currentTime}.</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+const book_id = "8bba937a-313c-4920-9b3b-ee2ef8f11528"
+
+function Books() {
   useEffect(() =>{
-    fetch('/time').then(res => res.json()).then(data =>{
-      setCurrentTime(data.time);
+    fetch(`/api/books/${book_id}`).then(response =>
+      response.json().then(data =>{
+      console.log(data);
     })
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the front-end of this project!
-        </p>
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+    );
+}, []);
+
+  return <div />
 }
 
-export default App;
+export default Books;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Books from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Books />
   </React.StrictMode>,
   document.getElementById('root')
 );
