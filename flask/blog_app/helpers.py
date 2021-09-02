63f5ec6370151f79411b2bdbc1d2d39d4b3fc6d3
backfromsolaris <porraszach@gmail.com>
2021-09-02 00:07:18 -0500

    clarify 2nd half of token_required

--- flask/blog_app/helpers.py
+++ flask/blog_app/helpers.py
@@ -14,6 +14,9 @@ def token_required(function_below):
         if not token:
             return jsonify({'message': 'Token is missing!'})
         
+        #########################################################################
+        ##### this error catch conditional needs to be clarified more to me #####
+        #########################################################################
         try:
             current_user_token = User.query.filter_by(token = token).first()
         except:
@@ -21,7 +24,8 @@ def token_required(function_below):
 
             if token != owner.token and secrets.compare_digest(token, owner.token):
                 return jsonify({'message': 'Invalid token!'})
-        
+        #########################################################################
+
         return function_below(current_user_token, *args, **kwargs)
     return decorated
 
