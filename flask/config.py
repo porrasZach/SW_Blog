6f8d5c2aa1dc43ffc785abeb7c3e79855f062b6b
backfromsolaris <porraszach@gmail.com>
2021-09-09 21:17:42 -0500

    restructure for deployment

--- flask/config.py
+++ flask/config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = "I hate sanddddddddddddddddddd454545666889090211111909"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
